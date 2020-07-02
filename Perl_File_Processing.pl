1ff3fee292f9735be5616c2323edcf6c5b48399e
Hassan <55107369+Hassan-Al@users.noreply.github.com>
2020-07-02 19:43:10 -0400

    Create Perl_File_Processing.pl

---
+++ Perl_File_Processing.pl
@@ -0,0 +1,75 @@
+#
+# Name: Hassan Al-Saffar
+#
+# Description: File that processing log file and filters based on user input and regular expression.
+#              This lab is seperated into six diffrent parts.
+
+use warnings;
+use strict;
+open (my $file, 'TestFile.txt') or die $!;
+my $line = <$file>;
+print $line;
+close $file;
+
+#-------------------------------------------------------------------------------------
+
+use warnings;
+use strict;
+open (my $file, 'TestFile.txt') or die $!;
+my @line = <$file>;
+print @line;
+close $file;
+
+#-------------------------------------------------------------------------------------
+
+use warnings;
+use strict;
+open (my $file, 'TestFile.txt') or die $!;
+my $line = <$file>;
+#print $line;
+print <$file>;
+close $file;
+
+#-------------------------------------------------------------------------------------
+
+use warnings;
+use strict;
+open (my $file, 'TestFile.txt') or die $!;
+while (<$file>)	{
+	print "$_";
+}
+close $file;
+
+#-------------------------------------------------------------------------------------
+
+use warnings;
+use strict;
+open (my $file, 'TestFile.txt') or die $!;
+my $lineno = 0;
+while (my $line = <$file>)	{
+	$lineno ++;
+	print "This is record ";
+	print $lineno;
+	print ": $line";
+} 
+print "
+The file containted $lineno records";
+close $file;
+
+#---------------------------------------------------------------------------------
+
+use warnings;
+use strict;
+open (my $file, 'TestFile.txt') or die $!;
+my $lineno = 0;
+while (my $line = <$file>)	
+{
+	if($line eq "line two\n"){next};
+	$lineno ++;
+	print "This is record ";
+	print $lineno;
+	print ": $line";
+	if($line eq "line four\n"){last};
+}
+print "The file containted $lineno records";
+close $file;
