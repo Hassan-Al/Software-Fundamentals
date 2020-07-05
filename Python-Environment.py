ff4a629651f55cc447347aa64fa0316062830cc6
Hassan <55107369+Hassan-Al@users.noreply.github.com>
2020-07-05 17:40:01 -0400

    Create Python-Environment.py

---
+++ Python-Environment.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------
+#Description: Takes user input and prints a friendly greeting.
+#
+#Perameters: None
+#
+#History:
+#Date        Name      Description
+#2017-1-2  H.Al-Saffar  Initial Creation
+#-------------------------------------------------------------
+
+name = input("Hello, please enter your name to log in...	")
+print("Hello", name)
